62a5718e8714f31b868886517e35491803bdbcdc
Rob Waller <r.t.waller@gmail.com>
2018-04-09 18:39:27 -0500

    Views made for individual posts. Started working on comments. Look forward to updating articles video for reference

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/blackhole');
 let db = mongoose.connection;
@@ -26,8 +27,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.set('port', (process.env.PORT || 8000));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
+
+// Home Route
 app.get('/', function(req, res) {
     Post.find({}, function(err, posts) {
         if(err){
@@ -39,6 +45,54 @@ app.get('/', function(req, res) {
         }
     });
 });
+
+// Add Post Route
+app.get('/posts/add', function(req, res) {
+    res.render('add_post');
+});
+
+// Add POST route for posts
+app.post('/posts/add', function(req, res) {
+    let post = new Post();
+    post.title = req.body.title;
+    post.author = req.body.author;
+    post.body = req.body.body;
+
+    post.save(function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+// Add POST route for commments
+app.post('/posts/comment/:id', function(req, res) {
+    console.log('Comment test');
+    // post.body = req.body.body;
+
+    // post.save(function(err) {
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     } else {
+    //         res.redirect('/');
+    //     }
+    // });
+});
+
+// Get Single Post
+app.get('/posts/:id', function(req, res) {
+    Post.findById(req.params.id, function (err, post) {
+        res.render('post', {
+            post: post
+        });
+    });
+});
+
+app.set('port', (process.env.PORT || 8000));
 
 app.listen(app.get('port'), function () {
     console.log('Server started');
