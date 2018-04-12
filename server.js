f4ef08504993caaaa33d540b7775f99446daf852
Rob Waller <r.t.waller@gmail.com>
2018-04-12 13:35:17 -0500

    New Article and Update Articles messaging working. Stopped at part 8 at 13:35

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost/blackhole');
 let db = mongoose.connection;
@@ -28,7 +31,39 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Set Static Folder
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+// Express Session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res)();
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+}));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -66,6 +101,7 @@ app.post('/posts/add', function(req, res) {
             console.log(err);
             return;
         } else {
+            req.flash('success', 'Post Added');
             res.redirect('/');
         }
     });
@@ -113,6 +149,7 @@ app.post('/posts/edit/:id', function(req, res) {
         post.body = req.body.body;
         post.save();
     });
+    req.flash('success', 'Post Updated');
     res.redirect('/posts/'+req.params.id);
 });
 
