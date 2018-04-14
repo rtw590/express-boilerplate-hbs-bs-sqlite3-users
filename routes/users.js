42460214cb67a54b84a961864b5f5a25153bb8fd
Rob Waller <r.t.waller@gmail.com>
2018-04-13 19:52:26 -0500

    Registering new user works

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 
 //Bring in Post Model
-let Post = require('../models/user');
+let User = require('../models/user');
 
 // Register Form
 router.get('/register', function(req, res) {
@@ -34,7 +34,29 @@ router.post('/register', function(req, res) {
             email:email,
             password:password
         });
+
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(newUser.password, salt, function(err, hash) {
+                if(err) {
+                    console.log(err);
+                }
+                newUser.password = hash;
+                newUser.save(function(err) {
+                    if(err){
+                        console.log(err);
+                        return;
+                    } else {
+                        req.flash('success', 'You are now registered');
+                        res.redirect('/users/login');
+                    }
+                })
+            });
+        });
     }
+});
+
+router.get('/login', function(req, res) {
+    res.render('login');
 });
 
 module.exports = router;
